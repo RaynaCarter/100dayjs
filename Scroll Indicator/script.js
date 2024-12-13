ba71c6c3cc01609e67c7dce320c38eac9e1e5660
Carter, Rayna (R.) <rcart174@ford.com>
2024-12-12 16:12:54 -0800

    code for Dec 12

---
+++ Scroll Indicator/script.js
@@ -0,0 +1,18 @@
+// smooth scrolling behavior with javascript 
+
+const links = document.querySelectorAll(".nav-list li a")
+
+for (link of links){
+    link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
+
+}
---
+++ Scroll To Top/script.js
@@ -0,0 +1,24 @@
+// SCROLL TO TOP
+const scrollBtn = document.querySelector(".top");
+const rootEl = document.documentElement;
+
+document.addEventListener("scroll", showBtn);
+scrollBtn.addEventListener("click", scrollTop);
+
+function showBtn() {
+    const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+
+    if (rootEl.scrollTop / scrollTotal > 0.3) {
+        scrollBtn.classList.add("show-btn");
+    } else {
+        scrollBtn.classList.remove("show-btn");
+
+    }
+}
+
+function scrollTop() {
+    rootEl.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}

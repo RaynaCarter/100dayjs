74a008771b75befb70f2eda826af51227728fc41
Carter, Rayna (R.) <rcart174@ford.com>
2024-12-17 14:52:10 -0800

    my version of pwGen logic

--- Password generator/script.js
+++ Password generator/script.js
@@ -23,49 +23,45 @@ subButton.addEventListener("click", genPw);
 
 function genPw() {
     let initialPw = empty;
-    // generates a random number from 0-25 (for alphabet) or 0-9 (for numbers and symbols)
+    let charOptions = "";
+    // generates a random number from 0-25 (for alphabet) or 0-10 (for numbers and symbols)
     // add initial character if checked 
     if(pUpper.checked) {
-        initialPw += uCase.charAt(Math.floor(Math.random() * 25));
+        initialPw += uCase.charAt(Math.floor(Math.random() * 26));
+        charOptions += uCase;
     }if(pLower.checked) {
-        initialPw += lCase.charAt(Math.floor(Math.random() * 25));
+        initialPw += lCase.charAt(Math.floor(Math.random() * 26));
+        charOptions += lCase;
     }if (pNumber.checked) {
         initialPw += number.charAt(Math.floor(Math.random() * 10));
+        charOptions += number;
     }if (pSymbol.checked) {
         initialPw += symbol.charAt(Math.floor(Math.random() * 10));
+        charOptions += symbol;
     }
-  
+    scramble(initialPw);
+    console.log(pLength.value);
+    for (let i = 0; i < (pLength.value-(initialPw.length)); i++) {
+        initialPw += charOptions.charAt(Math.floor(Math.random() * charOptions.length));     
+    }
     console.log(initialPw);
-      // create an array of strings from the initialPw 
-      let splitInitialPw = initialPw.split;
-      // interate through the indexes of the string 
-      for (let i = (splitInitialPw.length - 1); i > 0; i--) {
-          // chose a random number between 0 and arr length
-          let j = Math.floor(Math.random() * (i + 1));
-  
-          // replace every splitInitialPw[i] with a splitInitialPw[j]
-          [splitInitialPw[i], splitInitialPw[j]] = [splitInitialPw[j], splitInitialPw[i]]
-      }
-      console.log(splitInitialPw);
-      initialPw = splitInitialPw.join;
-      return initialPw;
-  
+    return initialPw
 }
 
 // I dont want the password to always start in a certain order so I'm gonna scramble it 
 // Using the Fisher-Yates shuffle Algorithm
-function scramble() {
+function scramble(initialPw) {
     // create an array of strings from the initialPw 
-    let splitInitialPw = initialPw.split;
-    // interate through the indexes of the string 
+    let splitInitialPw = initialPw.split("");
+    // interate through the indexes of the string working backwards
     for (let i = (splitInitialPw.length - 1); i > 0; i--) {
         // chose a random number between 0 and arr length
-        let j = Math.floor(Math.Random() * (i + 1));
+        let j = Math.floor(Math.random() * (i + 1));
 
-        // replace every splitInitialPw[i] with a splitInitialPw[j]
+        // swap every splitInitialPw[i] with a splitInitialPw[j]
         [splitInitialPw[i], splitInitialPw[j]] = [splitInitialPw[j], splitInitialPw[i]]
     }
-    console.log(splitInitialPw);
+    // console.log(splitInitialPw);
     initialPw = splitInitialPw.join;
     return initialPw;
 }
